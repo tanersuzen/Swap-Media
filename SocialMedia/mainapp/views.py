963d800534432044764edcd4a150a32ffcbf88d9
Taner Süzen <taner.suzen95@gmail.com>
2024-06-25 14:51:02 +0300

    commit after deac. Signup for Logged in User

--- SocialMedia/mainapp/views.py
+++ SocialMedia/mainapp/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render, redirect
 from . import models
 from django.urls import reverse, reverse_lazy
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.utils.decorators import method_decorator
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 
 
+def not_logged_in(user):
+    return not user.is_authenticated
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = "registration/signup.html"
+
+    @method_decorator(user_passes_test(not_logged_in, redirect_field_name=None, login_url="/"))
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
 
 def base(request):
